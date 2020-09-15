d74de53f02c3e2dd5221ff2e700b9d17e8ad24fb
anna_sorokina <asorokiina@gmail.com>
2020-09-15 19:27:23 +0300

    добавила штуку для формирования и добавления новостей + небольшие изменения в подвале

--- js/home-news.js
+++ js/home-news.js
@@ -27,17 +27,3 @@ function show_news(items) {
 	};
 }
 
-// Загрузка новой новости в json
-// function publish() {
-// 	var ntitle = document.getElementById("newsform-ntitle").value;  
-// 	var ncontent = document.getElementById("newsform-ncontent").value;
-
-// 	var d = new Date();
-// 	var months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября" , "декабря"]
-// 	var current_date = [d.getDate(), months[d.getMonth()], d.getFullYear()].join(" ")
-// 	var nitem = {"title": ntitle, "content": ncontent, "date": current_date};
-// 	alert(JSON.stringify(nitem));
-
-// }
-
-
